a72ba0bfe7f7169ca0a86365f95707f633546110
Boidushya Bhattacharya <boidushyabhattacharya@gmail.com>
2022-11-19 16:26:01 +0530

    fix: footer padding

--- src/components/Section4.tsx
+++ src/components/Section4.tsx
@@ -5,7 +5,7 @@ import { RoughNotation } from "react-rough-notation";
 const Section4 = () => {
 	const [show, setShow] = useState(false);
 	return (
-		<footer className="py-4 absolute bottom-0 grid place-items-center left-0 w-full">
+		<footer className="py-4 absolute bottom-0 grid place-items-center left-0 w-full px-8">
 			<p className="text-gray-600 lg:divide-x divide-gray-700 tracking-tight text-center">
 				<span className="inline-block pr-2">
 					&copy; copyright 2022{" "}
